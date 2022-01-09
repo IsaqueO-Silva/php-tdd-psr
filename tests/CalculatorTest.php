c1455b65397e0445ebddbaf6d51ab341f0437920
Isaque O. Silva <Isaque1945@hotmail.com>
2022-01-09 11:57:19 -0300

    The CalculatorTest class has fixed based in the PSR12

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -2,43 +2,51 @@
 
 /* This test class will be used as a
 base to the Calculator class */
+
+namespace Isaque\Calculator\tests;
+
 use PHPUnit\Framework\TestCase;
 
-class CalculatorTest extends TestCase {
-
+class CalculatorTest extends TestCase
+{
     private $fistNumber;
     private $secondNumber;
 
     #region
-    public function setFistNumber(float $fistNumber = 0): void {
+    public function setFistNumber(float $fistNumber = 0): void
+    {
         $this->fistNumber   = $this->validateNumber($fistNumber);
     }
 
-    public function getFistNumber(): float {
+    public function getFistNumber(): float
+    {
         return $this->fistNumber;
     }
 
-    public function setSecondNumber(float $secondNumber = 0): void {
+    public function setSecondNumber(float $secondNumber = 0): void
+    {
         $this->secondNumber = $this->validateNumber($secondNumber);
     }
 
-    public function getSecondNumber(): float {
+    public function getSecondNumber(): float
+    {
         return $this->secondNumber;
     }
 
-    public function validateNumber(float $number) {
+    public function validateNumber(float $number)
+    {
 
-        if(!(is_numeric($number))) {
+        if (!(is_numeric($number))) {
             throw new \TypeError('The number is invalid!');
             die;
-        }
-        else {
+        } else {
             return $number;
         }
     }
     #endregion
 
-    public function testTestTheSumBetweenTwoNumbers(): void {
+    public function testTestTheSumBetweenTwoNumbers(): void
+    {
 
         $this->setFistNumber(55);
         $this->setSecondNumber(92);
@@ -46,7 +54,8 @@ class CalculatorTest extends TestCase {
         $this->assertEquals(147, ($this->getFistNumber() + $this->getSecondNumber()));
     }
 
-    public function testTestTheSubtractionBetweenTwoNumbers(): void {
+    public function testTestTheSubtractionBetweenTwoNumbers(): void
+    {
 
         $this->setFistNumber(154);
         $this->setSecondNumber(76);
@@ -54,7 +63,8 @@ class CalculatorTest extends TestCase {
         $this->assertEquals(78, ($this->getFistNumber() - $this->getSecondNumber()));
     }
 
-    public function testTestTheMultiplicationBetweenTwoNumbers(): void {
+    public function testTestTheMultiplicationBetweenTwoNumbers(): void
+    {
 
         $this->setFistNumber(72);
         $this->setSecondNumber(6);
@@ -62,7 +72,8 @@ class CalculatorTest extends TestCase {
         $this->assertEquals(432, ($this->getFistNumber() * $this->getSecondNumber()));
     }
 
-    public function testTestTheDivisionBetweenTwoNumbers(): void {
+    public function testTestTheDivisionBetweenTwoNumbers(): void
+    {
 
         $this->setFistNumber(862);
         $this->setSecondNumber(2);
@@ -70,11 +81,11 @@ class CalculatorTest extends TestCase {
         $this->assertEquals(431, ($this->getFistNumber() / $this->getSecondNumber()));
     }
 
-    public function testTestNumberValidation(): void {
+    public function testTestNumberValidation(): void
+    {
 
         $this->expectException(TypeError::class);
 
         $this->setFistNumber('a');
     }
 }
-?>
